a07c9a505ac05ea82ef371f60c23408babb8ecab
Selomea Beyene <selomea.beyene@gmail.com>
2025-01-22 13:38:44 -0800

    what is your fav number with output

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello Idea, today is 1/22/2025");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your favorite number?");
+        //String name = scanner.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            double num = 0;
+            try{
+            num = scanner.nextDouble();
+        }catch (Exception e){
+            System.out.printf(num + "is not a number");
+            System.exit(0);
         }
-    }
-}
+        System.out.printf(num + "is a number");
+
+
+
+    }//end constructor
+}//end Main
